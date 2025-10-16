54165d61914de298e9dfcf328d21781ebc995e1f
Simeon Bird <sbird@ucr.edu>
2025-10-16 15:52:37 -0700

    Fix dependencies

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 
 setup(
     name="lyaemu",
-    version='1.0.1',
-    author="Simeon Bird, Martin Fernandez and Keir Rogers",
+    version='1.1.0',
+    author="Simeon Bird, Ming-Feng Ho, Martin Fernandez and Keir Rogers",
     author_email="spb@ucr.edu",
     license = "mit",
     url="http://github.com/sbird/lya_emulator",
@@ -11,7 +11,7 @@ setup(
     long_description="Module for easily generating emulators for the lyman alpha forest from simulations",
     long_description_content_type = "text/plain",
     packages = ['lyaemu', 'lyaemu.tests', 'lyaemu.SimulationRunner.SimulationRunner', 'lyaemu.meanT'],
-    requires=['numpy', 'pandas', 'fake_spectra','scipy', "GPy", "cobaya", "h5py"],
+    requires=['numpy', 'fake_spectra','scipy', "gpytorch", "cobaya", "h5py"],
     package_data = {
             'lyaemu': ['data/boss_dr*_data/*.dat','data/desi*/*.txt', 'data/kodiaq_squad/*.txt','data/xq100/*.csv','data/*', 'data/Gaikwad/Gaikwad_2020b_T0_Evolution_All_Statistics.txt'],
            },
